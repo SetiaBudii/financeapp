18e58d8b0f4339b55027650d8f9e2bfb5e4d5d67
YayangSB <yayang.setia.tif420@polban.ac.id>
2023-09-28 19:58:55 +0700

    add login

---
+++ Frontend/Reactjs/src/component/Userlist.jsx
@@ -0,0 +1,31 @@
+import React, {useState,useEffect} from 'react'
+import axios from 'axios'
+
+const Userlist = () => {
+ const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        loadUsers()
+    }, [])
+
+    const loadUsers = async () => {
+        const result = await axios.get("http://localhost:5000/users")
+        setUsers(result.data)
+    }
+    
+  return (
+    <div>
+      <h2>User Data</h2>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>
+            <p><strong>Username:</strong> {user.username}</p>
+            <p><strong>Password:</strong> {user.password}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Userlist
