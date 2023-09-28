a10d2427663763f3377566efcb16b625a60e4a85
YayangSB <yayang.setia.tif420@polban.ac.id>
2023-09-28 10:47:29 +0700

    update CRUD User

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -7,11 +7,11 @@ const prisma = new PrismaClient();
 // Function to create a new user
 export const createUser = async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const newUser = req.body;
     const user = await prisma.users.create({
-      data: {
-        username,
-        password,
+      data:{
+        username: newUser.username,
+        password: newUser.password
       },
     });
     res.json(user);
@@ -32,27 +32,7 @@ export const getAllUsers = async (req, res) => {
   }
 };
 
-// Function to fetch a single user by ID
-export const getUserById = async (req, res) => {
-  const { username } = req.params;
-  try {
-    const user = await prisma.users.findUnique({
-        where: {
-            username: username,
-        },
-    });
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-    res.json(user);
-  } catch (error) {
-    console.error('Error fetching user by ID:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-};
-
-
-export const getProductByUsername = async (req, res) => {
+export const getUserByUsername = async (req, res) => {
     try {
         const response = await prisma.users.findUnique({
             where: {
@@ -67,4 +47,82 @@ export const getProductByUsername = async (req, res) => {
     } catch (error) {
         res.status(500).json({ msg: error.message });
     }
+};
+
+export const deleteUserByUsername = async (req, res) => {
+  try {
+    const user = await prisma.users.findUnique({
+      where: {
+        username: req.params.username,
+      },
+    });
+    if (!user) {
+      res.status(404).json({ msg: 'User not found' });
+    } else {
+      await prisma.users.delete({
+        where: {
+          username: req.params.username,
+        },
+      });
+      res.status(200).json({ msg: 'User deleted successfully' });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+}
+
+// Function to update the username of a user by ID
+export const updatePasswordByUsername = async (req, res) => {
+  try {
+    const user = await prisma.users.findUnique({
+      where: {
+        username: req.params.username,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const updatedUser = await prisma.users.update({
+      where: {
+        username: req.params.username,
+      },
+      data: {
+        password: req.body.password,
+      },
+    });
+
+    res.json(updatedUser);
+  }
+  catch (error) {
+    console.error('Error updating username:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+  // try {
+  //   const user = await prisma.users.findUnique({
+  //     where: {
+  //       username: req.params.username,
+  //     },
+  //   });
+
+  //   if (!user) {
+  //     return res.status(404).json({ error: 'User not found' });
+  //   }
+
+  //   const updatedUser = await prisma.users.update({
+  //     where: {
+  //       username: username,
+  //     },
+  //     data: {
+  //       username: newUsername.username,
+  //       password: newUsername.password,
+  //     },
+  //   });
+
+  //   res.json(updatedUser);
+  // } catch (error) {
+  //   console.error('Error updating username:', error);
+  //   res.status(500).json({ error: 'Internal server error' });
+  // }
 };
