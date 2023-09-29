1206bd0416ef8a4086968e243ab7669df6d66594
YayangSB <yayang.setia.tif420@polban.ac.id>
2023-09-29 14:16:02 +0700

    update sidebar

--- Frontend/Reactjs/src/component/Sidebar.jsx
+++ Frontend/Reactjs/src/component/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 const Sidebar = () => {
-    const [style, setStyle] = useState("navbar-nav bg-gradient-primary sidebar sidebar-dark accordion");
+    const [style, setStyle] = useState("navbar-nav bg-gradient-dark sidebar sidebar-dark accordion");
     const changeStyle = () => {
         if (style === "navbar-nav bg-gradient-primary sidebar sidebar-dark accordion") {
             setStyle("navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled");
@@ -9,15 +9,12 @@ const Sidebar = () => {
             setStyle("navbar-nav bg-gradient-primary sidebar sidebar-dark accordion");
         }
     }
-    
+
     return (
         <>
             <ul className={style} id="accordionSidebar">
                 <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                    <div className="sidebar-brand-icon rotate-n-15">
-                        <i className="fas fa-laugh-wink"></i>
-                    </div>
-                    <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+                    <div className="sidebar-brand-text mx-1">M5-FinanceWeb</div>
                 </a>
                 <hr className="sidebar-divider my-0" />
                 <li className="nav-item active">
@@ -27,13 +24,12 @@ const Sidebar = () => {
                 </li>
                 <hr className="sidebar-divider" />
                 <div className="sidebar-heading">
-                    Interface
                 </div>
                 <li className="nav-item">
                     <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                         aria-expanded="true" aria-controls="collapseTwo">
-                        <i className="fas fa-fw fa-cog"></i>
-                        <span>Components</span>
+                        <i className="fas fa-fw fa-wallet"></i>
+                        <span>Wallet</span>
                     </a>
                     <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                         <div className="bg-white py-2 collapse-inner rounded">
@@ -47,8 +43,8 @@ const Sidebar = () => {
                 <li className="nav-item">
                     <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                         aria-expanded="true" aria-controls="collapseUtilities">
-                        <i className="fas fa-fw fa-wrench"></i>
-                        <span>Utilities</span>
+                        <i className="fas fa-fw fa-donate"></i>
+                        <span>Budgeting</span>
                     </a>
                     <div id="collapseUtilities" className="collapse" aria-labelledby="headingUtilities"
                         data-parent="#accordionSidebar">
@@ -64,11 +60,11 @@ const Sidebar = () => {
 
                 <hr className="sidebar-divider" />
 
-                <div className="sidebar-heading">
+                {/* <div className="sidebar-heading">
                     Addons
-                </div>
+                </div> */}
 
-                <li className="nav-item">
+                {/* <li className="nav-item">
                     <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
                         aria-expanded="true" aria-controls="collapsePages">
                         <i className="fas fa-fw fa-folder"></i>
@@ -86,29 +82,23 @@ const Sidebar = () => {
                             <a className="collapse-item" href="blank.html">Blank Page</a>
                         </div>
                     </div>
-                </li>
+                </li> */}
 
                 <li className="nav-item">
                     <a className="nav-link" href="charts.html">
-                        <i className="fas fa-fw fa-chart-area"></i>
-                        <span>Charts</span></a>
+                        <i className="fas fa-fw fa-receipt"></i>
+                        <span>Recap</span></a>
                 </li>
 
                 <li className="nav-item">
                     <a className="nav-link" href="tables.html">
-                        <i className="fas fa-fw fa-table"></i>
-                        <span>Tables</span></a>
+                        <i className="fas fa-fw fa-file-invoice-dollar"></i>
+                        <span>Report</span></a>
                 </li>
                 <hr className="sidebar-divider d-none d-md-block" />
 
                 <div className="text-center d-none d-md-inline">
                     <button className="rounded-circle border-0" id="sidebarToggle" name="sidebarToggle" onClick={changeStyle}></button>
-                </div>
-
-                <div className="sidebar-card d-none d-lg-flex">
-                    <img className="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="..." />
-                    <p className="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
-                    <a className="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
                 </div>
             </ul>
         </>
