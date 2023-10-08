de14dad5f008175b119bd629618ccd6cd38148a6
Velma32741 <142876320+HanriFajarRamadhan@users.noreply.github.com>
2023-10-08 11:54:38 +0700

    add button create wallet in view wallet

--- Frontend/Reactjs/src/pages/wallet.jsx
+++ Frontend/Reactjs/src/pages/wallet.jsx
@@ -3,44 +3,42 @@ import Sidebar from '../component/Sidebar';
 import Navbar from '../component/Navbar';
 import axios from "axios";
 import Swal from "sweetalert2";
-import Cookies from "js-cookie"
+import Cookies from "js-cookie";
+import ShowTipe from '../component/ShowTipe'; // Import ShowTipe component
+import WalletComp from '../component/WalletComp'; // Import WalletComp component
 
 const Wallet = () => {
     const [allWallet, setAllWallet] = useState([]);
     const [username, setUsername] = useState('');
-  
-    // const [newKategori, setNewKategori] = useState({
-    //     nama_kategori: 0,
-    //     budget: 0,
-    //     username: "",
-    //     id_kategori: 0,
-    // });
+    const [isCreateWalletPopupOpen, setCreateWalletPopupOpen] = useState(false); // State for controlling the popup
+    const [selectedTipe, setSelectedTipe] = useState('');
 
     useEffect(() => {
         const storedUsername = Cookies.get('username')
         if (storedUsername) {
             setUsername(storedUsername)
-            console.log('Stored Username:', storedUsername);
-            console.log(' Username:', username);
             loadKategori(storedUsername)
         }
-
-        
     }, []);
 
     const loadKategori = async (x) => {
         try {
             const result = await axios.get(`http://localhost:5000/wallets/${x}`, { validateStatus: false });
             setAllWallet(result.data.data);
-            console.log(result.data.data);
-            console.log(`http://localhost:5000/wallets/${x}`);
         } catch (error) {
             console.error("Error loading outcome data:", error);
         }
     }
-
     
 
+    const handleCreateWalletClick = () => {
+        // Toggle the visibility of the "Create Wallet" popup
+        setCreateWalletPopupOpen(!isCreateWalletPopupOpen);
+    }
+
+    const handleTipeChange = (value) => {
+        setSelectedTipe(value);
+      };
     return (
         <div id="wrapper">
             <Sidebar />
@@ -49,6 +47,7 @@ const Wallet = () => {
                     <Navbar />
                     <div className="container-fluid">
                         <div className="d-sm-flex align-items-center justify-content-between mb-4">
+                            <button onClick={handleCreateWalletClick} className="btn btn-primary">Create Wallet</button>
                         </div>
                         <div className="container-fluid">
                             <div className="card shadow mb-4 mt-5">
@@ -80,72 +79,17 @@ const Wallet = () => {
                     </div>
                 </div>
             </div>
-            
-            {/* <div className="modal fade" id="addoutcomemodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-                aria-hidden="true">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Add income</h5>
-                            <button className="close" type="button" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">×</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">                            
-                        <form onSubmit={AddNewOutcome}>
-                            <div className="form-group">
-                                <label htmlFor="id_wallet">Id Wallet</label>
-                                <input
-                                    type="number"
-                                    className="form-control"
-                                    id="id_wallet"
-                                    name="id_wallet"
-                                    value={newOutcome.id_wallet}
-                                    onChange={handleInputChange}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="amount">Amount</label>
-                                <input
-                                    type="number"
-                                    className="form-control"
-                                    id="amount"
-                                    name="amount"
-                                    value={newOutcome.amount}
-                                    onChange={handleInputChange}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="time_stamp">ID Kategori</label>
-                                <input
-                                    type="number"
-                                    className="form-control"
-                                    id="id_kategori"
-                                    name="id_kategori"
-                                    value={newOutcome.id_kategori}
-                                    onChange={handleInputChange}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="time_stamp">Time Stamp</label>
-                                <input
-                                    type="date"
-                                    className="form-control"
-                                    id="time_stamp"
-                                    name="time_stamp"
-                                    value={newOutcome.time_stamp}
-                                    onChange={handleInputChange}
-                                />
-                            </div>
-                            <button type="submit" className="btn btn-primary m-1">Submit</button>
-                            <button className="btn btn-secondary m-1 " type="button" data-dismiss="modal">Cancel</button>
-                        </form>
-                        </div>
-                    </div>
+            {isCreateWalletPopupOpen && (
+                <div className="create-wallet-popup">
+                    {/* ShowTipe component for selecting wallet type */}
+                    <ShowTipe onTipeChange={handleTipeChange} />
+
+                    {/* WalletComp component for creating the wallet */}
+                    <WalletComp username={username} selectedTipe={selectedTipe} />
                 </div>
-            </div> */}
+            )}
         </div>
     );
 }
 
-export default Wallet;
+export default Wallet;
