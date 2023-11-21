e75046d50126a666c6fa6075a8d3656b35755362
YayangSB <yayang.setia.tif420@polban.ac.id>
2023-11-21 18:49:21 +0700

    Fix Recap

--- Frontend/dev-dist/sw.js
+++ Frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5b010381'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jcja8783qu8"
+    "revision": "0.26f7clk2fe8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
