a88d076221c17f824ab7f91c2effead67c17c86b
YayangSB <yayang.setia.tif420@polban.ac.id>
2023-11-24 14:00:42 +0700

    update

--- Frontend/dev-dist/sw.js
+++ Frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6253340d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.64jlgqeukv"
+    "revision": "0.3eqflsuvug"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
