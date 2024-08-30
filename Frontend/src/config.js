19dc31eb76c00595a2b6d54b8f70338e9f8d46f7
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-08-30 12:36:46 +0700

    refactor: Update axios post URL to use template literals

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-    apiUrl: process.env.REACT_APP_API_URL, // Fallback to localhost for development
+    apiUrl: process.env.REACT_APP_API_URL || 'http://192.168.59.102:5005', // Fallback to localhost for development
   };
   
 export default config;
